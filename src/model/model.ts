11ea82d6cca0638c3bc39c828db55f1f4a081435
Lee-jeong-wook <126264218+Lee-jeong-wook@users.noreply.github.com>
2023-12-23 16:57:52 +0900

    id pw 값 바꾸면 전송은 아마 될 것...

--- src/model/model.ts
+++ src/model/model.ts
@@ -5,9 +5,12 @@ import puppeteer from "puppeteer";
 class Model {
     async #getData():Promise<void> {
         try {
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({headless: 'new'});
             const page = await browser.newPage();
             await page.goto('https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/');
+            page.type('#id', 'id');
+            page.type('#pw', 'pw');
+            page.click('.btn_login');
         } catch (error) {
             console.error(error);
         }
